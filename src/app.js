8eccd88ff4d83d39a51fa922ac8bf04f9ceac856
aysswarya-1 <a.aysswarya@gmail.com>
2026-02-16 17:45:36 +0530

    updated url

--- src/app.js
+++ src/app.js
@@ -4,10 +4,22 @@ import profileRoutes from "./routers/profileRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
+const allowedOrigins = [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL,
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
+
 app.use(express.json());
 
 app.use("/api/profile", profileRoutes);
