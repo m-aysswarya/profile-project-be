00d7333424e89b5b56be72bee29125552d979da0
aysswarya-1 <a.aysswarya@gmail.com>
2026-02-16 17:54:01 +0530

    updated url

--- src/app.js
+++ src/app.js
@@ -7,19 +7,11 @@ import profileRoutes from "./routers/profileRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
-const allowedOrigins = [
-    "http://localhost:5173",
-    process.env.FRONTEND_URL,
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: [
+        "http://localhost:5173",
+        "https://profile-project-pi.vercel.app/"
+    ],
     credentials: true
 }));
 
